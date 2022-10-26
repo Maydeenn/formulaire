18149b57d3d12b5bbb28b8b5bde1a8eadc8f8620
Maydeenn <115983798+Maydeenn@users.noreply.github.com>
2022-10-26 15:38:01 +0200

    Add files via upload

---
+++ TEMPLATE PORTFOLIO/index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+</head>
+<body class="main-content">
+    <header class="container header active" id="home">
+        <div class="header-content">
+            <div class="left-header">
+                <div class="h-shape"></div>
+                <div class="image">
+                    <img src="img/photomoi.jpg" alt="">
+                </div>
+            </div>
+            <div class="right-header">
+                <h1 class="name">
+                    Bonjour, je suis <span>Feraud Marc.</span>
+                    Un étudiant en 3ème année à la GBS.
+                </h1>
+                <p>
+                    J'ai rejoins la Gaming Business School en 3ème année.
+                    J'aime participer, gérer et amener à terme différents projets autour du gaming et de l'esport.
+
+                </p>
+                <div class="btn-con">
+                    <a href="img/Cv.pdf" target="_blank" class="main-btn">
+                        <span class="btn-text">Télécharger mon CV</span>
+                        <span class="btn-icon"><i class="fas fa-download"></i></span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="container about" id="about">
+            <div class="main-title">
+                <h2>A propos de  <span>moi</span><span class="bg-text">mes statistiques</span></h2>
+            </div>
+            <div class="about-container">
+                <div class="left-about">
+                    <h4>Quelques informations a mon sujet</h4>
+                    <p>
+                        Depuis deja 3 ans je suis impliqué de manière professionel dans le milieu de l'esport.
+                        Que ce soit à travers des associations, des évènements ou des projets scolaire j'ai pu augmenter mon réseau et mon expérience dans le domaine.
+                        
+                    </p>
+                    <div class="btn-con">
+                        <a href="img/Cv.pdf" target="_blank" class="main-btn">
+                            <span class="btn-text">Télécharger mon CV</span>
+                            <span class="btn-icon"><i class="fas fa-download"></i></span>
+                        </a>
+                    </div>
+                </div>
+                <div class="right-about">
+                    <div class="about-item">
+                        <div class="abt-text">
+                            <p class="large-text">12</p>
+                            <p class="small-text">Projets <br /> Completés</p>
+                        </div>
+                    </div>
+                    <div class="about-item">
+                        <div class="abt-text">
+                            <p class="large-text">3-+</p>
+                            <p class="small-text">Année <br /> d'experience</p>
+                        </div>
+                    </div>
+                    <div class="about-item">
+                        <div class="abt-text">
+                            <p class="large-text">C1</p>
+                            <p class="small-text">Niveau <br />d'anglais</p>
+                        </div>
+                    </div>
+                    <div class="about-item">
+                        <div class="abt-text">
+                            <p class="large-text">500</p>
+                            <p class="small-text">Score <br /> projet voltaire</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="about-stats">
+                <h4 class="stat-title">Mes compétences</h4>
+                <div class="progress-bars">
+                    <div class="progress-bar">
+                        <p class="prog-title">html</p>
+                        <div class="progress-con">
+                            <p class="prog-text">5%</p>
+                            <div class="progress">
+                                <span class="html"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-bar">
+                        <p class="prog-title">PowerPoint</p>
+                        <div class="progress-con">
+                            <p class="prog-text">95%</p>
+                            <div class="progress">
+                                <span class="css"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-bar">
+                        <p class="prog-title">php/sql</p>
+                        <div class="progress-con">
+                            <p class="prog-text">70%</p>
+                            <div class="progress">
+                                <span class="js"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="progress-bar">
+                        <p class="prog-title">Excel</p>
+                        <div class="progress-con">
+                            <p class="prog-text">50%</p>
+                            <div class="progress">
+                                <span class="react"></span>
+                            </div>
+                        </div>
+                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h4 class="stat-title">Mes expériences</h4>
+            <div class="timeline">
+                <div class="timeline-item">
+                    <div class="tl-icon">
+                        <i class="fas fa-briefcase"></i>
+                    </div>
+                    <p class="tl-duration">Decembre - avril 2022</p>
+                    <h5>Responsable partenariat<span> - Hegemony Esport</span></h5>
+                    <p>
+                        Demarchage de sponsort et création d'un dossier de sponsoring.
+                    </p>
+                </div>
+                <div class="timeline-item">
+                    <div class="tl-icon">
+                        <i class="fas fa-briefcase"></i>
+                    </div>
+                    <p class="tl-duration">Mai - Juillet 2021</p>
+                    <h5>Community Manager<span> - Quersus France</span></h5>
+                    <p>
+                        Gestions des réseaux et démarchage influenceurs.
+                    </p>
+                </div>
+                <div class="timeline-item">
+                    <div class="tl-icon">
+                        <i class="fas fa-briefcase"></i>
+                    </div>
+                    <p class="tl-duration">Juin - Aout 2020</p>
+                    <h5>Equipier polyvalent<span> - Quick St Victoret</span></h5>
+                    <p>
+                        Gestion de la clientèle, de la caisse et du drive.
+                    </p>
+                </div>
+                <div class="timeline-item">
+                    <div class="tl-icon">
+                        <i class="fas fa-briefcase"></i>
+                    </div>
+                    <p class="tl-duration">Avril - Juin 2020</p>
+                    <h5>Assistant chef de projet<span> - EvolveBDE</span></h5>
+                    <p>
+                        Administration de tournois et gestion influenceur.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="container" id="portfolio">
+            <div class="main-title">
+                <h2>Mon <span>Portfolio</span><span class="bg-text">Mon portfolio</span></h2>
+            </div>
+            <p class="port-text">
+                Voici un exemple de différents projet que j'ai pu réaliser depuis mon arrivé dans le monde du gaming.
+            </p>
+            <div class="portfolios">
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/Riotgame.JPG" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAE2lOk488A/rp0w9YHG-aP2eedeczlxVw/edit" target="_blank"
+                             class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/aeraofficiel.jpg" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAE_jznxDZc/O2UrqGXqbajjcTt9HifsDA/edit" target="_blank" class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/shangabi.jpg" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAEx5CUA8Qw/bLCg0aWLND6L2q8Vpr4BQQ/edit" target="_blank" class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/olympusrising.jpg" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAE5pT7OXwg/U6ZHF6WVeTGQiGpBjsk_tA/edit" target="_blank" class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/hegemony.jpg" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAEysfnMytc/sWe4OAGzmZz9QB4-UO5GUg/edit" target="_blank" class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="portfolio-item">
+                    <div class="image">
+                        <img src="img/elka.jpg" alt="">
+                    </div>
+                    <div class="hover-items">
+                        <h3>Project Source</h3>
+                        <div class="icons">
+                            <a href="https://www.canva.com/design/DAEvLtfh41g/tLPgcjoV6VEZvoQtq3mF_w/edit" target="_blank" class="icon">
+                                <i class="fab fa-google"></i>
+                            </a>
+                        </div>
+                    </div>
+            </div>
+        </section>
+        <section class="container" id="blogs">
+            <div class="blogs-content">
+                <div class="main-title">
+                    <h2>Mon <span>Blog</span><span class="bg-text">Mon blogue</span></h2>
+                </div>
+                <div class="blogs">
+                    <div class="blog">
+                        <img src="img/port6.jpg" alt="">
+                        <div class="blog-text">
+                            <h4>
+                                Mon projet php/sql
+                            </h4>
+                            <p>
+                                Voici le projet php
+                            </p>
+                        </div>
+                </div>
+            </div>
+        </section>
+        <section class="container contact" id="contact">
+            <div class="contact-container">
+                <div class="main-title">
+                    <h2>Mes <span>contacts</span><span class="bg-text">Contact</span></h2>
+                </div>
+                <div class="contact-content-con">
+                    <div class="left-contact">
+                        <h4>Mes contacts</h4>
+                        <p>
+                            Pour toutes questions ou informations,n'hésitez pas à me contacter
+                        </p>
+                        <div class="contact-info">
+                            <div class="contact-item">
+                                <div class="icon">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    <span>Location</span>
+                                </div>
+                                <p>
+                                    : Lyon, France
+                                </p>
+                            </div>
+                            <div class="contact-item">
+                                <div class="icon">
+                                    <i class="fas fa-envelope"></i>
+                                    <span>Email</span>
+                                </div>
+                                <p>
+                                    <span>: marc.feraud1@gmail.com</span>
+                                </p>
+                            </div>
+                            <div class="contact-item">
+                                <div class="icon">
+                                    <i class="fas fa-user-graduate"></i>
+                                    <span>Etudes</span>
+                                </div>
+                                <p>
+                                    <span>: Gaming Business School, Lyon</span>
+                                </p>
+                            </div>
+                            <div class="contact-item">
+                                <div class="icon">
+                                    <i class="fas fa-user-graduate"></i>
+                                    <span>Numéro de téléphone</span>
+                                </div>
+                                <p>
+                                    <span>: 0781511518</span>
+                                </p>
+                            </div>
+                            <div class="contact-item">
+                                <div class="icon">
+                                    <i class="fas fa-globe-africa"></i>
+                                    <span>Langues parlées</span>
+                                </div>
+                                <p>
+                                    <span>:French, English </span>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="contact-icons">
+                            <div class="contact-icon">
+                                <a href="www.facebook.com" target="_blank">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a href="#" target="_blank">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                                <a href="#" target="_blank">
+                                    <i class="fab fa-github"></i>
+                                </a>
+                                <a href="#" target="_blank">
+                                    <i class="fab fa-youtube"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right-contact">
+                        <form action="" class="contact-form" method="post">
+                            <div class="input-control i-c-2">
+                                <input type="text" name="nom" required placeholder="YOUR NAME">
+                                <input type="email" name="email" required placeholder="YOUR EMAIL">
+                            </div>
+                            <div class="input-control">
+                                <input type="text" name="subject" required placeholder="ENTER SUBJECT">
+                            </div>
+                            <div class="input-control">
+                                <textarea name="message" id="" cols="15" rows="8" placeholder="Message Here..."></textarea>
+                            </div>
+                            <div class="submit-btn">
+                                <input type="submit" class="main-btn">
+                                    <span class="btn-text">Envoyez le message</span>
+                                    <span class="btn-icon"><i class="fas fa-download"></i></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <div class="controls">
+        <div class="control active-btn" data-id="home" >
+            <i class="fas fa-home"></i>
+        </div>
+        <div class="control" data-id="about">
+            <i class="fas fa-user"></i>
+        </div>
+        <div class="control" data-id="portfolio">
+            <i class="fas fa-briefcase"></i>
+        </div>
+        <div class="control" data-id="contact">
+            <i class="fas fa-envelope-open"></i>
+        </div>
+    </div>
+    <div class="theme-btn">
+        <i class="fas fa-adjust"></i>
+    </div>
+    <script src="app.js"></script>
+    <?php
+if (isset($_POST["message"])) {
+    $retour = mail("marc.feraud1@gmail.com", $_POST["subject"], $_POST["nom"], $_POST["message"], "From:contact@marc.feraud.com\r\nReply-to:" . $_POST["email"]);
+    if ($retour) {
+    echo "<p>L'email a bien été envoyé.</p>";
+    }
+}
+?>
+</body>
+</html>
